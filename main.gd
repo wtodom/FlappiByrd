b65f8fa00c8ad7af4e40814f91ba3d395c8eccc0
Weston Odom <weston.odom@gmail.com>
2023-12-26 13:08:19 -0500

    technically working game

---
+++ main.gd
@@ -0,0 +1,53 @@
+extends Node
+
+@export var pipe_scene: PackedScene
+var score
+const GROUND_HEIGHT = 112
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$PipeTimer.stop()
+	$BackgroundMusic.stop()
+	$UI.show_game_over()
+
+func new_game():
+	score = 0
+	$UI.update_score(score)
+	$UI.show_message("Get Ready")
+	$Player.start($StartPosition.position)
+	$BackgroundMusic.play()
+	$StartTimer.start()
+
+
+func _on_pipe_timer_timeout():
+	var pipe = pipe_scene.instantiate()
+	var pipe_parts = pipe.get_children()
+	var delta_y = randi() % 450
+	var velocity = Vector2(-200, 0)
+	for part in pipe_parts:
+		if part.name != 'VisibleOnScreenNotifier2D':
+			part.linear_velocity = velocity
+			part.position.y -= delta_y
+	add_child(pipe)
+
+
+func _on_start_timer_timeout():
+	$PipeTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_player_score_increased():
+	score += 1
+	$ScoreSound.play()
+	$UI.update_score(score)
---
+++ player.gd
@@ -0,0 +1,52 @@
+extends Area2D
+
+signal crashed
+signal score_increased
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+		rotation = -PI/6
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+		rotation = PI/6
+	if not Input.is_anything_pressed():
+		rotation = 0
+
+	if Input.is_action_pressed('flap'):
+		pass
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func _on_body_entered(body):
+	# TODO: Why doesn't this always get hit?
+	match body.name:
+		'Gap':
+			score_increased.emit()
+		_:
+			hide() # Player disappears after being hit.
+			crashed.emit()
+			# Must be deferred as we can't change physics properties on a physics callback.
+			$CollisionShape2D.set_deferred("disabled", true)
